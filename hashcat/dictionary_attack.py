bc88294a2d16f629d15ab797487ed37323c91980
Ethan Ruchotzke <35568374+Ruchotzke@users.noreply.github.com>
2025-10-22 12:20:21 -0500

    Moving towards async

--- hashcat/dictionary_attack.py
+++ hashcat/dictionary_attack.py
@@ -1,14 +1,20 @@
+import asyncio
 import subprocess
 from textual.widgets import Log
 
 
-def try_dictionary_attack(md5_file: str, log: Log) -> list:
+async def try_dictionary_attack(md5_file: str, log: Log) -> list:
     """
     Attempt a dictionary attack against a list of passwords.
     :param md5_file: The name of a file containing hashes
     :return: A list of cracked passwords.
     """
     # Run hashcat
+    proc = await asyncio.create_subprocess_shell(
+        cmd=f"hashcat -m1000 {md5_file} -a0 /usr/share/seclists/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt --quiet --potfile-disabled",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
     result = subprocess.run(
         ['hashcat',
          '-m1000',
